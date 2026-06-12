0b49eee2b386aadf1315c5ea6dc87f63b88fef3d
Siddharth Nori <127247235+No-o-ri@users.noreply.github.com>
2026-06-12 12:19:09 -0400

    Renamme

--- launchers/default.sh
+++ launchers/default.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec python3 ./packages/lane_follower_node.py _autostart:=true
+dt-exec python3 ./packages/lane_follow.py _autostart:=true
 
 
 # ----------------------------------------------------------------------------
